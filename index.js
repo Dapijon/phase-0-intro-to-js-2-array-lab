e270d66eea98a5a9905dc9f88e5a020ba0eb680a
Leon Kinyanjui Maingi <leon.kinyanjui@student.moringaschool.com>
2023-09-17 22:17:44 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat(name) {
+    cats.push("Ralph")
+}
+function destructivelyPrependCat(name) {
+    cats.unshift("Bob")
+}
+function destructivelyRemoveLastCat(name) {
+    cats.pop(2)
+}
+function destructivelyRemoveFirstCat(name) {
+    cats.shift(1)
+}
+
+const copyCats = [...cats, "Bruno"];
+    
+
+
+
+console.log(cats);
